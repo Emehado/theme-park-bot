dd1a9b9008fabaad21b9ddd4988fdbf1449ad317
Bright Larson Nanevie <emehado@gmail.com>
2025-09-15 16:15:05 +0200

    add prompt and instructions

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,13 +1,21 @@
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
 import { conversationRepository } from '../repositories/conversation.repository';
 import OpenAI from 'openai';
+import template from '../prompts/chatbot.txt';
 
 interface ChatResponse {
   id: string;
   message: string;
 }
 
-const openaiKey = process.env.OPENAI_API_KEY;
+const parkInfo = readFileSync(
+  path.join(__dirname, '../prompts', 'WonderWorld.md'),
+  'utf-8',
+);
 
+const openaiKey = process.env.OPENAI_API_KEY;
+const instructions = template.replace('{{parkInfo}}', parkInfo);
 const client = new OpenAI({
   apiKey: openaiKey,
 });
@@ -20,6 +28,7 @@ export const chatService = {
     const response = await client.responses.create({
       input: prompt,
       model: 'gpt-4o-mini',
+      instructions,
       temperature: 0.2,
       max_output_tokens: 100,
       previous_response_id:
